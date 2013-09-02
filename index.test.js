a9c145f8948e09ed7666989bf0cf158576debc8c
shallker <imshallker@gmail.com>
2013-09-02 16:08:37 +0800

    Add test.

---
+++ index.test.js
@@ -0,0 +1,12 @@
+var ok = require('assert').ok;
+var eq = require('assert').stritEqual;
+var ObjectData = require('./index');
+
+
+ok(ObjectData.version)
+ok(ObjectData)
+ok(ObjectData.ObjectDate)
+ok(ObjectData.ObjectArray)
+ok(ObjectData.ObjectNumber)
+ok(ObjectData.ObjectString)
+ok(ObjectData.ObjectBoolean)
